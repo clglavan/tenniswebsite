c4ead6dd7552978797477e018cfd6740be231d76
Root <clglavan44@yahoo.com>
2014-05-14 14:45:57 +0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TennisWebsite::Application.config.secret_key_base = 'c6f13cb018fc60fe091f4b170d9462d57eed86f9af9762a63248264fd4e34ae1aacac796466aa14f7a63519f9522efdace03ed04bd395525a336369d30d96e45'
